0e6279be07083ba4bafcad23ee2f9f0ad6c8eee2
igoralves367 <igor.alves367@gmail.com>
2023-03-31 14:40:11 -0300

    Inicia Projeto

---
+++ src/main/java/br/com/attornatus/attornatuspessoaendereco/AttornatusPessoaEnderecoApplication.java
@@ -0,0 +1,13 @@
+package br.com.attornatus.attornatuspessoaendereco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttornatusPessoaEnderecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttornatusPessoaEnderecoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/attornatus/attornatuspessoaendereco/AttornatusPessoaEnderecoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.attornatus.attornatuspessoaendereco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttornatusPessoaEnderecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
