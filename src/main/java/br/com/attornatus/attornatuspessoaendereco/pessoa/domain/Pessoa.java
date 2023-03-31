69574ae024073b00e742c6ad18ce79052b4dc5ef
igoralves367 <igor.alves367@gmail.com>
2023-03-31 14:55:47 -0300

    Configurando Classe Pessoa e definindo dominio de negocio, finalizado

---
+++ src/main/java/br/com/attornatus/attornatuspessoaendereco/pessoa/domain/Pessoa.java
@@ -0,0 +1,35 @@
+package br.com.attornatus.attornatuspessoaendereco.pessoa.domain;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Entity
+public class Pessoa {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(columnDefinition = "uuid", name = "id", updatable = false, unique = true, nullable = false)
+	private UUID idPessoa;
+	@NotBlank
+	private String nomeCompleto;
+	@NotNull
+	private LocalDate dataNascimento;
+	
+	private LocalDateTime dataHoraDoCadastro;
+	private LocalDateTime dataHoraDaUltimaAlteracao;
+}
