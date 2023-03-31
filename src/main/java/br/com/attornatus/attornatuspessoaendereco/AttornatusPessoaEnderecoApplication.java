7ef07e94bfd3adc70858cb134c7a7863ccc3eb0a
igoralves367 <igor.alves367@gmail.com>
2023-03-31 14:49:48 -0300

    Adiciona anotações classe principal, projeto rondando, teste ok

--- src/main/java/br/com/attornatus/attornatuspessoaendereco/AttornatusPessoaEnderecoApplication.java
+++ src/main/java/br/com/attornatus/attornatuspessoaendereco/AttornatusPessoaEnderecoApplication.java
@@ -2,12 +2,15 @@ package br.com.attornatus.attornatuspessoaendereco;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/")
 @SpringBootApplication
 public class AttornatusPessoaEnderecoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AttornatusPessoaEnderecoApplication.class, args);
 	}
-
 }
