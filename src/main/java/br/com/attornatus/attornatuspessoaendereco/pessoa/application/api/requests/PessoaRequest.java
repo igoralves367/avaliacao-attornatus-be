5c90c47e73e5b317c5c2a4cd6d0369168e60faea
igoralves367 <igor.alves367@gmail.com>
2023-03-31 15:21:16 -0300

    Implementa Request e Response

--- src/main/java/br/com/attornatus/attornatuspessoaendereco/pessoa/application/api/requests/PessoaRequest.java
+++ src/main/java/br/com/attornatus/attornatuspessoaendereco/pessoa/application/api/requests/PessoaRequest.java
@@ -1,5 +1,14 @@
 package br.com.attornatus.attornatuspessoaendereco.pessoa.application.api.requests;
 
-public class PessoaRequest {
+import java.time.LocalDate;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import lombok.Value;
+@Value
+public class PessoaRequest {
+	@NotBlank
+	private String nomeCompleto;
+	@NotNull
+	private LocalDate dataNascimento;
 }
