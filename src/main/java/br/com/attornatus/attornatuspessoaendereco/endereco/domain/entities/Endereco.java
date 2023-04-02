f4d011870e234a4d8722e9d68783df3a58c944f6
igoralves367 <igor.alves367@gmail.com>
2023-04-01 21:03:39 -0300

    Configurando class domain e definindo dominio de negocio, finalizado

---
+++ src/main/java/br/com/attornatus/attornatuspessoaendereco/endereco/domain/entities/Endereco.java
@@ -0,0 +1,42 @@
+package br.com.attornatus.attornatuspessoaendereco.endereco.domain.entities;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import br.com.attornatus.attornatuspessoaendereco.endereco.domain.enums.TipoEndereco;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Entity
+public class Endereco {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(columnDefinition = "uuid", name = "id", updatable = false, unique = true, nullable = false)
+	private UUID idEndereco;
+	@NotNull
+	@Column(columnDefinition = "uuid", name = "idPessoa", nullable = false)
+	private UUID idPessoaCadastro;
+	@NotBlank
+	private String lagradouro;
+	@NotBlank
+	private String cep;
+	@NotBlank
+	private String numero;
+	@NotBlank
+	private String cidade;
+	@NotNull
+	private TipoEndereco tipoEndereco;
+	
+	private LocalDateTime dataHoraDoCadastro;
+}
