161dd5c23812e346e76bbb638fb49e583f64c251
igoralves367 <igor.alves367@gmail.com>
2023-04-01 22:01:40 -0300

    Implementa ListaEnderecosResponse e cria um construtor para essa classe

--- src/main/java/br/com/attornatus/attornatuspessoaendereco/endereco/application/api/responses/ListaEnderecosResponse.java
+++ src/main/java/br/com/attornatus/attornatuspessoaendereco/endereco/application/api/responses/ListaEnderecosResponse.java
@@ -1,5 +1,24 @@
 package br.com.attornatus.attornatuspessoaendereco.endereco.application.api.responses;
 
+import java.util.UUID;
+import br.com.attornatus.attornatuspessoaendereco.endereco.domain.entities.Endereco;
+import br.com.attornatus.attornatuspessoaendereco.endereco.domain.enums.TipoEndereco;
+import lombok.Value;
+@Value
 public class ListaEnderecosResponse {
-
+	private UUID idEndereco;
+	private String lagradouro;
+	private String cep;
+	private String numero;
+	private String cidade;
+	private TipoEndereco tipoEndereco;
+	
+	public ListaEnderecosResponse(Endereco endereco) {
+		this.idEndereco = endereco.getIdEndereco();
+		this.lagradouro = endereco.getLagradouro();
+		this.cep = endereco.getCep();
+		this.numero = endereco.getNumero();
+		this.cidade = endereco.getCidade();
+		this.tipoEndereco = endereco.getTipoEndereco();
+	}	
 }
