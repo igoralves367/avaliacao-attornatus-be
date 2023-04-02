6b824d6d9e5ba3f7c77fa74fc04e22237ebaf480
igoralves367 <igor.alves367@gmail.com>
2023-04-02 15:40:30 -0300

    Teste do metodo Altera Pessoa, teste Ok

--- src/test/java/br/com/attornatus/attornatuspessoaendereco/pessoa/application/service/PessoaApplicationServiceTest.java
+++ src/test/java/br/com/attornatus/attornatuspessoaendereco/pessoa/application/service/PessoaApplicationServiceTest.java
@@ -18,6 +18,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import br.com.attornatus.attornatuspessoaendereco.endereco.application.repository.EnderecoRepository;
+import br.com.attornatus.attornatuspessoaendereco.pessoa.application.api.requests.EditaPessoaRequest;
 import br.com.attornatus.attornatuspessoaendereco.pessoa.application.api.requests.PessoaRequest;
 import br.com.attornatus.attornatuspessoaendereco.pessoa.application.api.responses.ListaPessoasResponse;
 import br.com.attornatus.attornatuspessoaendereco.pessoa.application.api.responses.PessoaResponse;
@@ -62,5 +63,25 @@ class PessoaApplicationServiceTest {
         assertEquals(pessoas.size(), listaPessoasResponse.size());
         assertEquals(pessoas.get(0).getNomeCompleto(), listaPessoasResponse.get(0).getNomeCompleto());
         assertEquals(pessoas.get(1).getNomeCompleto(), listaPessoasResponse.get(1).getNomeCompleto());
+    }
+    
+    @Test
+    public void testEditaPessoa() {
+        UUID idPessoa = UUID.randomUUID();
+        EditaPessoaRequest editaPessoaRequest = new EditaPessoaRequest("Igor Alves", LocalDate.of(1997, 6, 3));
+        
+        Pessoa pessoa = new Pessoa();
+        pessoa.setIdPessoa(idPessoa);
+        pessoa.setNomeCompleto("Igor Alves Martins");
+        pessoa.setDataNascimento(LocalDate.of(1990, 1, 1));
+        
+        when(pessoaRepository.consultaPessoaAtravesId(idPessoa)).thenReturn(pessoa);
+
+        pessoaApplicationService.editaPessoa(idPessoa, editaPessoaRequest);
+
+        verify(pessoaRepository).salva(pessoa);
+
+        assertEquals(editaPessoaRequest.getNomeCompleto(), pessoa.getNomeCompleto());
+        assertEquals(editaPessoaRequest.getDataNascimento(), pessoa.getDataNascimento());
     }
 }
