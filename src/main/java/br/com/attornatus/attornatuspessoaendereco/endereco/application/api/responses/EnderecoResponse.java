a6caecd8482e841cf6b6e2981e5a3a39c0dab1ab
igoralves367 <igor.alves367@gmail.com>
2023-04-01 21:33:16 -0300

    Implementa Request e Response

--- src/main/java/br/com/attornatus/attornatuspessoaendereco/endereco/application/api/responses/EnderecoResponse.java
+++ src/main/java/br/com/attornatus/attornatuspessoaendereco/endereco/application/api/responses/EnderecoResponse.java
@@ -1,5 +1,8 @@
 package br.com.attornatus.attornatuspessoaendereco.endereco.application.api.responses;
 
+import java.util.UUID;
+import lombok.Value;
+@Value
 public class EnderecoResponse {
-
+	private UUID idEndereco;
 }
